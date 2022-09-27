5b8a021d3a4ebf992a3499239365c87a8d6d7084
michelleangelica <18220045@mahasiswa.itb.ac.id>
2022-09-27 22:53:08 +0700

    fastapi fix

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from http.client import HTTPException
 import json
 from fastapi import FastAPI
-
-with open ("mahasiswa.json", "r") as read_file:
-    data =  json.load(read_file)
+with open("mahasiswa.json", "r") as read_file: 
+    data = json.load(read_file)
 app = FastAPI()
-@app.get("/mahasiswa/{NIM_mhs}")
-async def read_mahasiswa(NIM_mhs: int):
+@app.get('/mahasiswa/{item_id}')
+async def read_mahasiswa(item_id: int):
     for mahasiswa_item in data['mahasiswa']:
-        if mahasiswa_item['NIM'] == NIM_mhs:
+        if mahasiswa_item['NIM'] == item_id:
             return mahasiswa_item
-    raise HTTPException(status_code=404, detail=f'Not found')
+    raise HTTPException(
+        status_code=404, detail=f'Item not found')
