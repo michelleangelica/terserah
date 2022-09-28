6e89b50033718b062e533e90964ef559dd6f4837
michelleangelica <18220045@mahasiswa.itb.ac.id>
2022-09-28 09:51:20 +0700

    fastapi

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from http.client import HTTPException
 import json
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class mahasiswa_item(BaseModel):
+    Nama: str
+    NIM: int
+
+data = []
+
 with open("mahasiswa.json", "r") as read_file: 
     data = json.load(read_file)
+
 app = FastAPI()
-@app.get('/mahasiswa/{item_id}')
-async def read_mahasiswa(item_id: int):
-    for mahasiswa_item in data['mahasiswa']:
-        if mahasiswa_item['NIM'] == item_id:
-            return mahasiswa_item
+@app.get('/mahasiswa/{NIM}')
+async def read_mahasiswa(NIM: int):
+    for mahasiswa in data['mahasiswa']:
+        if mahasiswa['NIM'] == NIM:
+            return mahasiswa
     raise HTTPException(
         status_code=404, detail=f'Item not found')
+
+@app.post("/mahasiswa")
+async def add_mahasiswa(mahasiswa_baru: mahasiswa_item):
+    new_mahasiswa = [
+        {
+            "NIM": mahasiswa_baru.NIM,
+            "NAMA": mahasiswa_baru.Nama
+        }
+    ]
+    data['mahasiwa'].append(new_mahasiswa)
+    return data
